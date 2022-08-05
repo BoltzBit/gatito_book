317f981abeb19b4f40168f0025c0c5b190c3c75c
mateus.silva <mateus.silva@iatec.com>
2022-08-05 08:45:00 -0300

    Add home component

---
+++ front/src/app/home/home.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HomeRoutingModule } from './home-routing.module';
+import { BodyComponent } from './body/body.component';
+
+@NgModule({
+  declarations: [
+    BodyComponent
+  ],
+  imports: [
+    CommonModule,
+    HomeRoutingModule
+  ]
+})
+export class HomeModule { }
