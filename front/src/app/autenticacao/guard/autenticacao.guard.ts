d5ca71f4b51869dfd50d448e45a618aa3941a32b
mateus.silva <mateus.silva@iatec.com>
2022-09-02 07:43:10 -0300

    Add guard

---
+++ front/src/app/autenticacao/guard/autenticacao.guard.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { CanLoad, Route, Router, UrlSegment, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { UsuarioService } from '../usuario/usuario.service';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class AutenticacaoGuard implements CanLoad {
+    constructor(
+        private _usuarioService: UsuarioService,
+        private _router: Router) {
+
+    }
+
+    canLoad(
+        route: Route,
+        segments: UrlSegment[]): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+        
+        if(!this._usuarioService.estaLogado()){
+            this._router.navigate(['']);
+            return false;
+        }
+
+        return true;
+    }
+}
