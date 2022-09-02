4bcef39cb285c2fb3604c29baf56cf9ce7a216ec
mateus.silva <mateus.silva@iatec.com>
2022-09-02 08:30:44 -0300

    Add login guard

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AutenticacaoGuard } from './autenticacao/guard/autenticacao.guard';
+import { LoginGuard } from './autenticacao/guard/login.guard';
 
 const routes: Routes = [
     { path: '', pathMatch: 'full', redirectTo: 'home' },//full informa pra tirar os espaços
     {
         path: 'home',
-        loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
+        loadChildren: () => import('./home/home.module').then(m => m.HomeModule),
+        canLoad: [LoginGuard]
     },
     {
         path: 'animais',
-        loadChildren: () => import('./animais/animais.module').then(m => m.AnimaisModule)
+        loadChildren: () => import('./animais/animais.module').then(m => m.AnimaisModule),
+        canLoad: [AutenticacaoGuard]
     }
 ];
 
