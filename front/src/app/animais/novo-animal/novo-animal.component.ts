3b11d31e2fa9d95f2d9e675a60192cfbc5f4eead
mateus.silva <mateus.silva@iatec.com>
2022-09-12 08:54:15 -0300

    Improvement novo animal

--- front/src/app/animais/novo-animal/novo-animal.component.ts
+++ front/src/app/animais/novo-animal/novo-animal.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
-  selector: 'app-novo-animal',
-  templateUrl: './novo-animal.component.html',
-  styleUrls: ['./novo-animal.component.scss']
+    selector: 'app-novo-animal',
+    templateUrl: './novo-animal.component.html',
+    styleUrls: ['./novo-animal.component.scss']
 })
 export class NovoAnimalComponent implements OnInit {
+    public formularioAnimal!: FormGroup;
+    public file!: File;
+    public preview!: string;
+    public percentualConcluido: number = 0;
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
+    public upload(): void{
+
+    }
+
+    public gravarArquivo(arquivo: any): void{
+
+    }
 }
