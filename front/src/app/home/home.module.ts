97ecbf8a8d7ceb507f07b17575b64bbce8033fb8
mateus.silva <mateus.silva@iatec.com>
2022-09-15 08:04:49 -0300

    Improvements shared

--- front/src/app/home/home.module.ts
+++ front/src/app/home/home.module.ts
@@ -4,10 +4,9 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { BodyComponent } from './body/body.component';
 import { LoginComponent } from './body/login/login.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MensagemModule } from '../componentes/mensagem/mensagem.module';
+import { FormsModule } from '@angular/forms';
 import { NovoUsuarioComponent } from './body/novo-usuario/novo-usuario.component';
-import { SharedModule } from '../shared/shared/shared.module';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
     declarations: [
